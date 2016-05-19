77ee8de4f8cd49994817b0d6603b992206008573
fuhaiwei <fuhaiwei@126.com>
2016-05-19 20:18:55 +0800

    add remove discs

---
+++ manual/remove/remove_discs.sql
@@ -0,0 +1,41 @@
+DELETE FROM disc_list_discs WHERE discs_id = 195;
+DELETE FROM disc_record WHERE disc_id = 195;
+DELETE FROM disc_sakura WHERE disc_id = 195;
+DELETE FROM disc_rank WHERE disc_id = 195;
+DELETE FROM disc WHERE id = 195;
+
+DELETE FROM disc_list_discs WHERE discs_id = 242;
+DELETE FROM disc_record WHERE disc_id = 242;
+DELETE FROM disc_sakura WHERE disc_id = 242;
+DELETE FROM disc_rank WHERE disc_id = 242;
+DELETE FROM disc WHERE id = 242;
+
+DELETE FROM disc_list_discs WHERE discs_id = 219;
+DELETE FROM disc_record WHERE disc_id = 219;
+DELETE FROM disc_sakura WHERE disc_id = 219;
+DELETE FROM disc_rank WHERE disc_id = 219;
+DELETE FROM disc WHERE id = 219;
+
+DELETE FROM disc_list_discs WHERE discs_id = 222;
+DELETE FROM disc_record WHERE disc_id = 222;
+DELETE FROM disc_sakura WHERE disc_id = 222;
+DELETE FROM disc_rank WHERE disc_id = 222;
+DELETE FROM disc WHERE id = 222;
+
+DELETE FROM disc_list_discs WHERE discs_id = 218;
+DELETE FROM disc_record WHERE disc_id = 218;
+DELETE FROM disc_sakura WHERE disc_id = 218;
+DELETE FROM disc_rank WHERE disc_id = 218;
+DELETE FROM disc WHERE id = 218;
+
+DELETE FROM disc_list_discs WHERE discs_id = 221;
+DELETE FROM disc_record WHERE disc_id = 221;
+DELETE FROM disc_sakura WHERE disc_id = 221;
+DELETE FROM disc_rank WHERE disc_id = 221;
+DELETE FROM disc WHERE id = 221;
+
+DELETE FROM disc_list_discs WHERE discs_id = 220;
+DELETE FROM disc_record WHERE disc_id = 220;
+DELETE FROM disc_sakura WHERE disc_id = 220;
+DELETE FROM disc_rank WHERE disc_id = 220;
+DELETE FROM disc WHERE id = 220;
